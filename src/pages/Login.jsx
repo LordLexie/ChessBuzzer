8683be59c5cac8f84b230ee69cd1b8728e7ba2aa
LordLexie <muigaifrancis@gmail.com>
2024-10-31 12:01:07 +0300

    fix: fixed bug arising from import of useAuth hook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import useAuth from '../../hooks/useAuth';
+import React, { useState, useEffect } from 'react';
+import useAuth from '../hooks/useAuth';
 
 import axios from 'axios';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import LoginPageWrapper from "../components/layouts/LoginPageWrapper";
 
 function Login() {
-    return (
-        <LoginPageWrapper>
-            <div className="card card-outline card-primary">
+
+  const navigate = useNavigate();
+  const { setAuth } = useAuth()
+
+  const [loginInput, setLogin] = useState({
+    email: '',
+    password: '',
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setLogin({ ...loginInput, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <LoginPageWrapper>
+      <div className="card card-outline card-primary">
         <div className="card-header text-center">
           <span className="h2"><b>Chess</b>Buzzer</span>
         </div>
         <div className="card-body">
           <p className="login-box-msg">Sign in to start your session</p>
-    
+
           <form action="../../index3.html" method="post">
             <div className="input-group mb-3">
               <input type="email" className="form-control" placeholder="Email" />
@@ -34,16 +49,16 @@ function Login() {
               </div>
             </div>
             <div className="row">
-              <div className="col-8">                
+              <div className="col-8">
               </div>
-              
+
               <div className="col-4">
                 <button type="submit" className="btn btn-primary btn-block">Sign In</button>
               </div>
-              
+
             </div>
-          </form>         
-    
+          </form>
+
           <p className="mb-1">
             <a href="forgot-password.html">Forgot my password</a>
           </p>
@@ -51,10 +66,10 @@ function Login() {
             <a href="register.html" className="text-center">Register</a>
           </p>
         </div>
-        
+
       </div>
-        </LoginPageWrapper>
-    )
+    </LoginPageWrapper>
+  )
 }
 
 export default Login
