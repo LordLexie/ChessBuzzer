b21a17aae207a16a21c44f738e9e67f13aeddf3f
LordLexie <muigaifrancis@gmail.com>
2024-10-31 09:57:40 +0300

    feat: create a footer component
    the footer component will hold footer elements and links

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer(){
+    return (
+    <footer className="main-footer">
+
+        <div className="float-right d-none d-sm-inline">
+        Anything you want
+        </div>
+
+        <strong>Powered by </strong> Alexie Digital Studios
+
+    </footer>
+    )
+}
+
+export default Footer
