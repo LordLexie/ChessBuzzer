dc78d49615dbbacc6ef3686dbd35120ecb4f9423
LordLexie <muigaifrancis@gmail.com>
2024-11-01 17:19:46 +0300

    refactor: re introduce private components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
+
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Cookies from 'js-cookie';
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
 import Login from './pages/Login.jsx'
 import PlayerDashboard from './pages/PlayerDashboard.jsx'
+import PrivateRoutes from './components/authentication/PrivateRoutes.jsx'
+
+axios.defaults.baseURL = "http://127.0.0.1:8888";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.withCredentials = true;
+
+axios.interceptors.request.use(function (config) {
+  //const token = Cookies.get("Authorization");
+  //config.headers.Authorization = token ? `Authorization=${token}` : '';
+  return config;
+});
 
 function App() {
 
   return (
-    <Router> 
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        
+        <Route element={<PrivateRoutes />} >
         <Route path="/dashboard" element={<PlayerDashboard />} />
+        </Route>
+
+
       </Routes>
     </Router>
   )
