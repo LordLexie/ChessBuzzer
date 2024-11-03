ee5098fdafa3e08204df56958d3e0d64e3c8244c
LordLexie <muigaifrancis@gmail.com>
2024-11-03 10:31:25 +0300

    feat: list user wallets from the backend

--- src/pages/PlayerDashboard.jsx
+++ src/pages/PlayerDashboard.jsx
@@ -4,6 +4,43 @@ import TopNav from '../components/layouts/TopNav'
 import Sidebar from '../components/layouts/Sidebar'
 import Footer from '../components/layouts/Footer';
 import Aside from '../components/layouts/Aside';
+import { jwtDecode } from "jwt-decode";
+import Cookies from 'js-cookie';
+import axios from 'axios';
+
+function FormatTime(dateString) {
+
+  let date = new Date(dateString);
+
+  // Format the date to a human-friendly format
+  let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    //hour: '2-digit',
+    //minute: '2-digit',
+    //second: '2-digit',
+    //timeZoneName: 'short'
+  };
+
+  let formattedDate = date.toLocaleString('en-US', options);
+  return formattedDate
+}
+
+function formatMoney(num) {
+  // Convert the number to a string, split on the decimal point
+  const [integerPart, decimalPart] = num.toString().split('.');
+  
+  // Format the integer part with commas
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  
+  // Limit the decimal part to 3 digits if it exists
+  const formattedDecimal = decimalPart ? '.' + decimalPart.slice(0, 3) : '';
+  
+  // Combine both parts
+  return formattedInteger + formattedDecimal;
+}
+
 function PlayerDashboard() {
 
   const [userDetails, setUserDetails] = useState({
@@ -11,16 +48,55 @@ function PlayerDashboard() {
     avatar: '../assets/dist/img/user1-128x128.jpg',
   });
 
-  const [userChallenges,setChallenges] = useState([])
+  const [userChallenges, setChallenges] = useState([])
+  const [userWallets, setWallets] = useState([])
+  const decoded_token = jwtDecode(Cookies.get("Authorization"))
+
+
 
   useEffect(() => {
 
     setUserDetails({
       username: localStorage.getItem("username").toLocaleUpperCase(),
-      avatar:localStorage.getItem("avatar"),
+      avatar: localStorage.getItem("avatar"),
     })
 
-    console.log("We are in the players dashboard")
+
+    // Fetch the player challenges / game
+    axios.get(`api/v1/challenge-martrix/player_games/${decoded_token.sub}`,).then(res => {
+
+
+      if (res.data.status === "Ok") {
+
+        setChallenges(res.data.data)
+
+      }
+      else if (res.data.status === 401) {
+        swal('Warning', res.data.message, "warning")
+      }
+      else {
+        setLogin({ ...loginInput, error_list: res.data.validation_errors })
+      }
+
+    })
+
+    axios.get(`api/v1/player-wallet/user/${decoded_token.sub}`,).then(res => {
+
+
+      if (res.data.status === "Ok") {
+
+        setWallets(res.data.data)
+
+      }
+      else if (res.data.status === 401) {
+        swal('Warning', res.data.message, "warning")
+      }
+      else {
+        setLogin({ ...loginInput, error_list: res.data.validation_errors })
+      }
+
+    })
+
   }, [])
 
   return (
@@ -49,7 +125,7 @@ function PlayerDashboard() {
 
                 <div className="card card-widget widget-user shadow">
 
-                  
+
 
                   <div className="widget-user-image">
                     <img className="img-circle elevation-2" src={userDetails.avatar} alt="User Avatar" />
@@ -63,35 +139,37 @@ function PlayerDashboard() {
 
                 </div>
 
-                <div className="small-box bg-info">
-                  <div className="inner">
-                    <h3>KES 1,500</h3>
+                {userWallets.map((wallet, index) => (
+                  <div className="small-box bg-info">
+                    <div className="inner">
+                      <h4>{wallet.currency} {formatMoney(wallet.balance)}</h4>
 
-                    <p>Account Balance</p>
-                  </div>
-                  <div className="icon">
-                    <i className="ion ion-bag"></i>
-                  </div>
-
-                  <div className="row" style={{ paddingBottom: '5px' }}>
-                    <div style={{ width: '50%' }}>
-                      <span className="small-box-footer" style={{ padding: '5px' }}>
-                        <span className='btn btn-default btn-sm'>
-                          Deposit <span className='fas fa-arrow-up'></span>
-                        </span>
-                      </span>
+                      <p>Account Balance</p>
+                    </div>
+                    <div className="icon">
+                      <i className="ion ion-bag"></i>
                     </div>
 
-                    <div style={{ width: '50%' }}>
-                      <span className="small-box-footer" style={{ padding: '5px' }}>
-                        <span className='btn btn-default btn-sm'>
-                          Withdraw <span className='fas fa-arrow-down'></span>
+                    <div className="row" style={{ paddingBottom: '5px' }}>
+                      <div style={{ width: '50%' }}>
+                        <span className="small-box-footer" style={{ padding: '5px' }}>
+                          <span className='btn btn-default btn-sm'>
+                            Deposit <span className='fas fa-arrow-up'></span>
+                          </span>
                         </span>
-                      </span>
-                    </div>
+                      </div>
 
+                      <div style={{ width: '50%' }}>
+                        <span className="small-box-footer" style={{ padding: '5px' }}>
+                          <span className='btn btn-default btn-sm'>
+                            Withdraw <span className='fas fa-arrow-down'></span>
+                          </span>
+                        </span>
+                      </div>
+
+                    </div>
                   </div>
-                </div>
+                ))}
 
               </div>
 
@@ -113,23 +191,25 @@ function PlayerDashboard() {
                     <table className="table table-hover text-nowrap">
                       <thead>
                         <tr>
-                          <th style={{ fontSize: '15px' }} >#</th>
-                          <th style={{ fontSize: '15px' }}>Date</th>
-                          <th style={{ fontSize: '15px' }}>Challenge</th>
-                          <th style={{ fontSize: '15px' }}>Prize Money</th>
-                          <th style={{ fontSize: '15px' }}>Status</th>
-                          <th style={{ fontSize: '15px' }}>Action</th>
+                          <th style={{ fontSize: '14px' }} >#</th>
+                          <th style={{ fontSize: '14px' }}>Date</th>
+                          <th style={{ fontSize: '14px' }}>Challenge</th>
+                          <th style={{ fontSize: '14px' }}>Entry Fee</th>
+                          <th style={{ fontSize: '14px' }}>Status</th>
+                          <th style={{ fontSize: '14px' }}><span className='fa fa-edit'></span></th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr >
-                          <td style={{ fontSize: '14px' }} >1</td>
-                          <td style={{ fontSize: '14px' }}>11-7-2014</td>
-                          <td style={{ fontSize: '14px' }} >1ff9f787-4d4e-4319-b0a5-58bcea075ab3</td>
-                          <td style={{ fontSize: '14px' }}>1,200</td>
-                          <td style={{ fontSize: '14px' }}><span className="tag tag-warning">Pending</span></td>
-                          <td style={{ fontSize: '14px' }}>Edit</td>
-                        </tr>
+                        {userChallenges.map((challenge, index) => (
+                          <tr key={index}>
+                            <td style={{ fontSize: '14px' }} >{index + 1}</td>
+                            <td style={{ fontSize: '14px' }}>{FormatTime(challenge.CreatedAt)}</td>
+                            <td style={{ fontSize: '14px' }} >{challenge.Description}</td>
+                            <td style={{ fontSize: '14px' }}>{challenge.Currency} {challenge.EntryFee}</td>
+                            <td style={{ fontSize: '14px' }}>{challenge.Status}</td>
+                            <td style={{ fontSize: '14px' }}><span className='fa fa-edit'></span></td>
+                          </tr>
+                        ))}
                       </tbody>
                     </table>
                   </div>
